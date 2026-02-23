45dad7cd7aa718c03cca9a13124fa6bc5a9ae3fb
seanfinnessy <seanfinnessy@icloud.com>
2026-02-23 17:39:47 -0500

    small update to start grabbing hero assets

--- backend/internal/deadlock/deadlock.go
+++ backend/internal/deadlock/deadlock.go
@@ -8,20 +8,25 @@ import (
 )
 
 type PlayerMatchHistoryEntry struct {
-	AccountID int32 `json:"account_id"`
-	PlayerKills int32 `json:"player_kills"` 
-	PlayerDeaths int32 `json:"player_deaths"` 
-	PlayerAssists int32 `json:"player_assists"` 
-	HeroID int32 `json:"hero_id"`
+	AccountID     int32 `json:"account_id"`
+	PlayerKills   int32 `json:"player_kills"`
+	PlayerDeaths  int32 `json:"player_deaths"`
+	PlayerAssists int32 `json:"player_assists"`
+	HeroID        int32 `json:"hero_id"`
 }
 
-func GetMatchHistoryURL(userId string) string {
-	return "https://api.deadlock-api.com/v1/players/" + userId + "/match-history?only_stored_history=true"
+type HeroAssets struct {
+	HeroID int32  `json:"id"`
+	Name   string `json:"name"`
+}
+
+func ConstructGetMatchHistoryURL(userId string) string {
+	return PlayersURL + userId + MatchHistorySuff
 }
 
 func GetMatches(userId string, numMatches int) ([]PlayerMatchHistoryEntry, error) {
-	var playerHistory[] PlayerMatchHistoryEntry
-	url := GetMatchHistoryURL(userId)
+	var playerHistory []PlayerMatchHistoryEntry
+	url := ConstructGetMatchHistoryURL(userId)
 	res, errGet := http.Get(url)
 	if errGet != nil || res.StatusCode != 200 {
 		return []PlayerMatchHistoryEntry{}, fmt.Errorf("Issue calling API: %s", url)
@@ -38,6 +43,30 @@ func GetMatches(userId string, numMatches int) ([]PlayerMatchHistoryEntry, error
 	if errUnmarshal != nil {
 		return []PlayerMatchHistoryEntry{}, errUnmarshal
 	}
-	
+
 	return playerHistory[:numMatches], nil
+}
+
+func GetHeroAssets() error {
+	// Either grab from DB, or if version changes, we would update then return those values
+	url := AssetsURL + HeroAssetsSuff
+	res, ok := http.Get(url)
+	if ok != nil || res.StatusCode != 200 {
+		return fmt.Errorf("Issue calling API: %s", url)
+	}
+
+	bodyBytes, errDecode := io.ReadAll(res.Body)
+	if errDecode != nil {
+		return errDecode
+	}
+
+	var heroAssets HeroAssets
+	errUnmarshal := json.Unmarshal(bodyBytes, &heroAssets)
+	if errUnmarshal != nil {
+		return errUnmarshal
+	}
+
+	// Write values to database
+	// Implement some logic eventually that grabs values from DB..
+	// If version changes, we would need to recall this API, update DB.
 }
---
+++ backend/internal/deadlock/endpoints.go
@@ -0,0 +1,8 @@
+package deadlock
+
+const (
+	AssetsURL string = "https://assets.deadlock-api.com/v2"
+	PlayersURL string = "https://api.deadlock-api.com/v1/players"
+	MatchHistorySuff string = "/match-history?only_stored_history=true"
+	HeroAssetsSuff string = "/heroes"
+)
