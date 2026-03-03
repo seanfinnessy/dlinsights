9d20defeb1da7d8e68ef85297cf7212ee91a7549
seanfinnessy <seanfinnessy@icloud.com>
2026-03-03 02:13:40 -0500

    started adding file reading for game start

--- app.go
+++ app.go
@@ -3,13 +3,84 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/seanfinnessy/dlinsights/internal/deadlock"
 	"github.com/seanfinnessy/dlinsights/internal/login"
 	wailsruntime "github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+func addWatcherRecursive(watcher *fsnotify.Watcher, root string) error {
+	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return watcher.Add(path)
+		}
+
+		return nil
+	})
+}
+
+func getDLchanges() {
+	rootPath := "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Deadlock"
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Watch existing folders
+	err = addWatcherRecursive(watcher, rootPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Watching all folders under:", rootPath)
+
+	for {
+		select {
+
+		case event := <-watcher.Events:
+
+			fmt.Println("Event:", event)
+
+			// New directory → watch it
+			if event.Op&fsnotify.Create == fsnotify.Create {
+				if info, err := os.Stat(event.Name); err == nil && info.IsDir() {
+					watcher.Add(event.Name)
+					fmt.Println("New directory watched:", event.Name)
+				}
+			}
+
+			// File written → read contents
+			if event.Op&fsnotify.Write == fsnotify.Write {
+
+				if info, err := os.Stat(event.Name); err == nil && !info.IsDir() {
+					if event.Name.includes("reconnect.dat") { // TODO: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Deadlock\\game\\citadel\\reconnect.dat" is created and written at game start, and removed at end
+						fmt.Println("Found game")
+					}
+					data, err := os.ReadFile(event.Name)
+					if err == nil {
+						fmt.Println("File updated:", event.Name)
+						fmt.Println(string(data))
+					}
+				}
+			}
+
+		case err := <-watcher.Errors:
+			fmt.Println("Watcher error:", err)
+		}
+	}
+}
 
 type App struct {
 	ctx context.Context
@@ -21,6 +92,8 @@ func NewApp() *App {
 
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	getDLchanges()
 }
 
 func (a *App) GetMatches(steamId string, numMatches int) ([]deadlock.PlayerMatchHistoryEntry, error) {
@@ -69,7 +142,7 @@ func (a *App) SteamLogin() error {
 			wailsruntime.EventsEmit(a.ctx, "steam:authed", steamId)
 			w.Write([]byte("Login successful! You can close this window."))
 		}
-		
+
 		go srv.Shutdown(context.Background())
 	})
 
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/seanfinnessy/dlinsights
 
-go 1.23
+go 1.24.0
 
 require (
+	github.com/fsnotify/fsnotify v1.9.0
 	github.com/wailsapp/wails/v2 v2.11.0
 	github.com/yohcop/openid-go v1.0.1
 )
@@ -26,6 +27,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/samber/lo v1.49.1 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/tkrajina/go-reflector v0.5.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
@@ -33,6 +35,6 @@ require (
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 )
